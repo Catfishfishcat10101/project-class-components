1b80cf22b2ea2001376fd4f2612c599a436c2172
William Johnson <catfishfishcat10101@gmail.com>
2023-09-09 14:42:05 -0500

    Update Form.js

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
 
 export default class Form extends React.Component {
+  // Setting Up State
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.handleAdd(this.state.input);
+
+    // Clear the input field
+    this.setState({
+      input: ""
+    });
+  }
+
+  // Making Handle Change Event
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    });
+  }
+
   render() {
     return (
-      <div>
-        Form
-      </div>
-    )
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.input} // Bind input value to state
+          onChange={this.handleChange}
+        />
+        <button type="submit">Add</button>
+      </form>
+    );
   }
 }
